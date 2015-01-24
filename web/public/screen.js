b2ef96fb5b6db8e81cab3b6d0c8b4092468f44b4
Liu Xinan <xinan@me.com>
2015-01-24 21:23:28 +0800

    Fixed containsPoint

--- web/public/screen.js
+++ web/public/screen.js
@@ -14,10 +14,9 @@ $(function () {
   socket.on('user joined', function (userId) {
     console.log('New user joined: ', userId.newUserId);
     users.push({ key: userId.newUserId, value: null });
-    console.log(users);
   });
 
-  circle = new fabric.Circle({
+  var circle = new fabric.Circle({
     radius: 100,
     left: 100,
     top: 100,
@@ -25,21 +24,22 @@ $(function () {
   });
   canvas.add(circle);
 
-
+  fabric.Object.prototype.containsPoint = function(point) {
+    var horiz_in = point.x >= this.getLeft() && point.x <= this.getLeft() + this.getWidth();
+    var vert_in = point.y >= this.getTop() && point.y <= this.getTop() + this.getHeight();
+    return horiz_in && vert_in;
+  };
 
   socket.on('user input touchdown', function (data) {
-    console.log('down');
     var point = new fabric.Point(data.x, data.y);
     canvas.getObjects().forEach(function (obj) {
       if (obj.containsPoint(point)) {
-        console.log(obj);
         users[data.userId] = obj;
       }
     });
   });
 
   socket.on('user input touchmove', function (data) {
-    console.log(data.x + ', ' + data.y);
     var target = users[data.userId];
     if (target) {
       target.set({
@@ -51,7 +51,6 @@ $(function () {
   });
 
   socket.on('user input touchup', function (data) {
-    console.log('up');
     users[data.userId] = null;
   })
 
