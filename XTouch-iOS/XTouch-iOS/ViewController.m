016d6d86764f99dd5cf6e294b87ef4c810f069b8
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-01-25 00:53:08 +0800

    Make camera full screen

--- XTouch-iOS/XTouch-iOS/ViewController.m
+++ XTouch-iOS/XTouch-iOS/ViewController.m
@@ -42,6 +42,7 @@
     [super viewDidLoad];
     
     // Do any additional setup after loading the view, typically from a nib.
+    imageView.frame = self.view.frame;
     
     AVCaptureSession *session = [[AVCaptureSession alloc] init];
     session.sessionPreset = AVCaptureSessionPreset352x288;
